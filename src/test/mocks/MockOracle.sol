c92d6008009dfa877cb115b4251f05a2cf9c8bb9
Anton Cheng <antonassocareer@gmail.com>
2022-05-23 00:15:41 +0200

    misc: add MockOracle

--- src/test/mocks/MockOracle.sol
+++ src/test/mocks/MockOracle.sol
@@ -4,23 +4,29 @@ pragma solidity ^0.8.13;
 import "src/interfaces/IOracle.sol";
 
 contract MockOracle is IOracle {
-    
     uint256 public spotPrice;
     uint256 public expiryPrice;
 
-    function getSpotPrice(address /*_underlying*/, address /*_strike*/) view external returns (uint256) {
+    function getSpotPrice(
+        address, /*_underlying*/
+        address /*_strike*/
+    ) external view returns (uint256) {
         return spotPrice;
     }
 
-    function getPriceAtExpiry(address /*_underlying*/, address /*_strike*/, uint256 /*_expiry*/ ) external view returns (uint256) {
+    function getPriceAtExpiry(
+        address, /*_underlying*/
+        address, /*_strike*/
+        uint256 /*_expiry*/
+    ) external view returns (uint256) {
         return expiryPrice;
     }
 
     function setSpotPrice(uint256 _mockedSpotPrice) external {
-      spotPrice = _mockedSpotPrice;
+        spotPrice = _mockedSpotPrice;
     }
 
     function setExpiryPrice(uint256 _mockedExpiryPrice) external {
-      expiryPrice = _mockedExpiryPrice;
+        expiryPrice = _mockedExpiryPrice;
     }
 }
