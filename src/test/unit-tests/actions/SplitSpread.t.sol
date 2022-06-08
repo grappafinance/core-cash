86a7eba556ee9ec7fd4c40a6955a0e56193aa761
Anton Cheng <antonassocareer@gmail.com>
2022-06-08 17:26:04 +0200

    test: add put spread tests

--- src/test/unit-tests/actions/SplitSpread.t.sol
+++ src/test/unit-tests/actions/SplitSpread.t.sol
@@ -70,3 +70,65 @@ contract TestSplitCallSpread is Fixture {
         grappa.execute(address(this), actions);
     }
 }
+
+contract TestSplitPutSpread is Fixture {
+    uint256 public expiry;
+    uint256 public strikePriceHigh = 2000 * UNIT;
+    uint256 public strikePriceLow = 1900 * UNIT;
+    uint256 public depositAmount = 100 * UNIT;
+    uint256 public amount = 1 * UNIT;
+
+    function setUp() public {
+        usdc.mint(address(this), 1000_000 * 1e6);
+        usdc.approve(address(grappa), type(uint256).max);
+
+        expiry = block.timestamp + 7 days;
+
+        oracle.setSpotPrice(3000 * UNIT);
+
+        // mint a 2000-1900 debit spread
+        uint256 tokenId = getTokenId(TokenType.PUT_SPREAD, productId, expiry, strikePriceHigh, strikePriceLow);
+
+        ActionArgs[] memory actions = new ActionArgs[](2);
+        actions[0] = createAddCollateralAction(productId, address(this), depositAmount);
+        actions[1] = createMintAction(tokenId, address(this), amount);
+        grappa.execute(address(this), actions);
+    }
+
+    function testSplitPutSpread() public {
+        // split
+        ActionArgs[] memory actions = new ActionArgs[](2);
+        actions[0] = createSplitAction(TokenType.PUT_SPREAD, address(this));
+        actions[1] = createAddCollateralAction(productId, address(this), depositAmount * 5); // will need to add collateral
+        grappa.execute(address(this), actions);
+
+        // check result
+        (, uint256 shortPutId, , , , ) = grappa.marginAccounts(address(this));
+        (, , , uint64 longStrike, uint64 shortStrike) = parseTokenId(shortPutId);
+
+        assertEq(longStrike, strikePriceHigh);
+        assertEq(shortStrike, 0);
+    }
+
+    function testSplitCallSpreadCreateNewCallToken() public {
+        // split
+        ActionArgs[] memory actions = new ActionArgs[](2);
+        actions[0] = createSplitAction(TokenType.PUT_SPREAD, address(this));
+        actions[1] = createAddCollateralAction(productId, address(this), depositAmount * 5); // will need to add collateral
+        grappa.execute(address(this), actions);
+
+        // check result
+        uint256 expectedTokenId = getTokenId(TokenType.PUT, productId, expiry, strikePriceLow, 0);
+
+        assertEq(option.balanceOf(address(this), expectedTokenId), amount);
+    }
+
+    function testCannotSplitCallSpreadWithoutAddingCollateral() public {
+        // only split
+        ActionArgs[] memory actions = new ActionArgs[](1);
+        actions[0] = createSplitAction(TokenType.PUT_SPREAD, address(this));
+
+        vm.expectRevert(AccountUnderwater.selector);
+        grappa.execute(address(this), actions);
+    }
+}
