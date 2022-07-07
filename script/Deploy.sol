a17888e940088ee31c4e7301d74cf25540324501
Anton Cheng <antonassocareer@gmail.com>
2022-07-07 11:31:50 +0200

    build: use ffi to execute node script to find salt (#63)
    * build: use `ffi` to execute node script to find salt
    
    * chore: make eth-create2-calculator a dev-dependency package
    
    * chore: lean logs
    
    * build: consider nonce
    
    * chore: add vscode config for convenience
    
    * chore: lint

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -4,6 +4,7 @@ pragma solidity =0.8.13;
 import "forge-std/Script.sol";
 
 import "openzeppelin/utils/Create2.sol";
+import "openzeppelin/utils/Strings.sol";
 
 import "src/core/OptionToken.sol";
 import "src/core/SimpleMargin/MarginAccount.sol";
@@ -34,25 +35,28 @@ contract Deploy is Script, Utilities {
             address marginAccount
         )
     {
+        uint256 nonce = vm.getNonce(msg.sender);
+        console.log("nonce", nonce);
+
         console.log("deploying with", msg.sender);
         // deploy deployer to use create2
-        Create2Deployer deployer = new Create2Deployer(); // nonce 0
+        Create2Deployer deployer = new Create2Deployer(); // nonce
 
         // prepare bytecode for Oracle
-        address chainlinkPricerAddr = addressFrom(msg.sender, 4);
+        address chainlinkPricerAddr = predictAddress(msg.sender, nonce + 4);
         bytes memory oCreationCode = type(Oracle).creationCode;
         bytes memory oBytecode = abi.encodePacked(oCreationCode, abi.encode(chainlinkPricerAddr, address(0)));
-        oracle = deployWithLeadingZeros(deployer, 0, oBytecode, 2); // nonce 1
+        oracle = deployWithLeadingZeros(deployer, 0, oBytecode, 2); // nonce + 1
         console.log("oracle", oracle);
-        console.log("primary pricer: ", address(Oracle(oracle).primaryPricer()));
+        console.log("primary pricer set (predicted): ", address(Oracle(oracle).primaryPricer()));
 
         // prepare bytecode for MarginAccount
-        address optionTokenAddr = addressFrom(msg.sender, 3);
-        console.log("optionTokenAddr", optionTokenAddr);
+        address optionTokenAddr = predictAddress(msg.sender, nonce + 3);
+        console.log("optionToken address (prediction)", optionTokenAddr);
         // deploy MarginAccount
         bytes memory maCreationCode = type(MarginAccount).creationCode;
         bytes memory maBytecode = abi.encodePacked(maCreationCode, abi.encode(optionTokenAddr, oracle));
-        marginAccount = deployWithLeadingZeros(deployer, 0, maBytecode, 1); // nonce 2
+        marginAccount = deployWithLeadingZeros(deployer, 0, maBytecode, 2); // nonce + 2
         console.log("marginAccount", marginAccount);
 
         // deploy optionToken directly, just so the address is the same as predicted by `create` (optionTokenAddr) 
@@ -61,29 +65,34 @@ contract Deploy is Script, Utilities {
         
         address chainlinkPricer = address(new ChainlinkPricer(oracle)); // nonce: 4
         console.log("chainlinkPricer", chainlinkPricer);
-    }   
+    }
 
-    function deployWithLeadingZeros(Create2Deployer deployer, uint256 value, bytes memory creationCode, uint8 zerosBytes) 
+    function deployWithLeadingZeros(
+        Create2Deployer deployer, 
+        uint256 value, 
+        bytes memory creationCode,
+        uint8 zerosBytes
+    ) 
         internal 
         returns (address addr) 
     {
-        uint8 bits = zerosBytes * 8;
-        uint160 bound = type(uint160).max;
-
-        // solhint-disable-next-line
-        assembly {
-            bound := shr(bits, bound)
-        }
-        uint256 salt;
+        // pass in codeHash so the js library doesn't have to do it in every iteration
         bytes32 codeHash = keccak256(creationCode);
 
-        while (true) {
-            address prediction = Create2.computeAddress(bytes32(salt), codeHash , address(deployer));
-            if (uint160(prediction) < bound) break;
-            unchecked {
-                salt++;
-            }
-        }
-        addr = deployer.deploy(value, creationCode, bytes32(salt));
+        string[] memory inputs = new string[](5);
+        inputs[0] = "node";
+        inputs[1] = "script/findSalt.js";
+        
+        inputs[2] = toString(address(deployer));
+        inputs[3] = toString(codeHash);
+        inputs[4] = Strings.toString(uint256(zerosBytes));
+
+        bytes memory res = vm.ffi(inputs);
+        bytes32 salt = abi.decode(res, (bytes32));
+
+        console.log("deploying with salt:");
+        emit log_bytes32(salt);
+
+        addr = deployer.deploy(value, creationCode, salt);
     }
 }
