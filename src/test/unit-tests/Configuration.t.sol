b10fda27f6cb4d250ea33c4a37d7ec13f3000791
Anton Cheng <antonassocareer@gmail.com>
2022-07-11 18:01:29 +0200

    test: make tests public

--- src/test/unit-tests/Configuration.t.sol
+++ src/test/unit-tests/Configuration.t.sol
@@ -5,11 +5,21 @@ pragma solidity ^0.8.13;
 import {Fixture} from "src/test/shared/Fixture.t.sol";
 
 import "src/config/constants.sol";
+import "src/core/SimpleMargin/MarginAccount.sol";
 
 contract OwnerConfiguration is Fixture {
-    function testSettingConfigUpdateState() internal {
-        uint32 dUpper = 7 days;
-        uint32 dLower = 1 days;
+    event ProductConfigurationUpdated(
+        uint32 productId,
+        uint32 dUpper,
+        uint32 dLower,
+        uint32 rUpper,
+        uint32 rLower,
+        uint32 volMul
+    );
+
+    function testSettingConfigUpdateState() public {
+        uint32 dUpper = 1000_000; // 11.57 days, 1000^2
+        uint32 dLower = 90_000; // 1.04 days, 30^2
         uint32 rUpper = uint32(UNIT);
         uint32 rLower = uint32(UNIT);
         uint32 volMultiplier = uint32(UNIT);
@@ -34,13 +44,25 @@ contract OwnerConfiguration is Fixture {
         assertEq(_volMultiplier, volMultiplier);
 
         // squared value is set correctly
-        assertEq(_sqrtDupper*_sqrtDupper, _dUpper);
-        assertEq(_sqrtDLower*_sqrtDLower, _dLower);
+        assertEq(_sqrtDupper * _sqrtDupper, _dUpper);
+        assertEq(_sqrtDLower * _sqrtDLower, _dLower);
     }
 
-    function testCannotUpdateProductConfigFromNonOwner() internal {
+    function testEmitEventWhenSetConfig() public {
+        uint32 dUpper = 7 days;
+        uint32 dLower = 1 days;
+        uint32 rUpper = uint32(UNIT / 2);
+        uint32 rLower = uint32(UNIT / 10);
+        uint32 volMultiplier = 1300000; // 130%
+
+        vm.expectEmit(false, false, false, true, address(grappa));
+        emit ProductConfigurationUpdated(productId, dUpper, dLower, rUpper, rLower, volMultiplier);
+        grappa.setProductMarginConfig(productId, dUpper, dLower, rUpper, rLower, volMultiplier);
+    }
+
+    function testCannotUpdateProductConfigFromNonOwner() public {
         vm.startPrank(alice);
-        vm.expectRevert("Owner: caller is not the owner");
+        vm.expectRevert("Ownable: caller is not the owner");
         grappa.setProductMarginConfig(productId, 0, 0, uint32(UNIT), uint32(UNIT), uint32(UNIT));
         vm.stopPrank();
     }
