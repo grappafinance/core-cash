bff2a5abf16bfaa73f95aa9fb31a725d9f6cdaa2
Anton Cheng <antonassocareer@gmail.com>
2022-07-13 15:30:05 +0200

    chore: mark contracts as abstract to be excluded from coverage command (#75)

--- src/test/shared/ActionHelper.sol
+++ src/test/shared/ActionHelper.sol
@@ -6,17 +6,15 @@ import "src/config/types.sol";
 
 import "src/libraries/TokenIdUtil.sol";
 
-/**
- * todo: change to internal after coverage fix
- */
-contract ActionHelper {
+// todo: make this a library?
+abstract contract ActionHelper {
     function getTokenId(
         TokenType tokenType,
         uint32 productId,
         uint256 expiry,
         uint256 longStrike,
         uint256 shortStrike
-    ) public pure returns (uint256 tokenId) {
+    ) internal pure returns (uint256 tokenId) {
         tokenId = TokenIdUtil.formatTokenId(
             tokenType,
             productId,
@@ -27,7 +25,7 @@ contract ActionHelper {
     }
 
     function parseTokenId(uint256 tokenId)
-        public
+        internal
         pure
         returns (
             TokenType tokenType,
@@ -44,12 +42,12 @@ contract ActionHelper {
         uint8 collateralId,
         address from,
         uint256 amount
-    ) public pure returns (ActionArgs memory action) {
+    ) internal pure returns (ActionArgs memory action) {
         action = ActionArgs({action: ActionType.AddCollateral, data: abi.encode(from, uint80(amount), collateralId)});
     }
 
     function createRemoveCollateralAction(uint256 amount, address recipient)
-        public
+        internal
         pure
         returns (ActionArgs memory action)
     {
@@ -60,7 +58,7 @@ contract ActionHelper {
         uint256 tokenId,
         address recipient,
         uint256 amount
-    ) public pure returns (ActionArgs memory action) {
+    ) internal pure returns (ActionArgs memory action) {
         action = ActionArgs({action: ActionType.MintShort, data: abi.encode(tokenId, recipient, uint64(amount))});
     }
 
@@ -68,19 +66,23 @@ contract ActionHelper {
         uint256 tokenId,
         address from,
         uint256 amount
-    ) public pure returns (ActionArgs memory action) {
+    ) internal pure returns (ActionArgs memory action) {
         action = ActionArgs({action: ActionType.BurnShort, data: abi.encode(tokenId, from, uint64(amount))});
     }
 
-    function createMergeAction(uint256 tokenId, address from) public pure returns (ActionArgs memory action) {
+    function createMergeAction(uint256 tokenId, address from) internal pure returns (ActionArgs memory action) {
         action = ActionArgs({action: ActionType.MergeOptionToken, data: abi.encode(tokenId, from)});
     }
 
-    function createSplitAction(TokenType tokenType, address recipient) public pure returns (ActionArgs memory action) {
+    function createSplitAction(TokenType tokenType, address recipient)
+        internal
+        pure
+        returns (ActionArgs memory action)
+    {
         action = ActionArgs({action: ActionType.SplitOptionToken, data: abi.encode(tokenType, recipient)});
     }
 
-    function createSettleAction() public pure returns (ActionArgs memory action) {
+    function createSettleAction() internal pure returns (ActionArgs memory action) {
         action = ActionArgs({action: ActionType.SettleAccount, data: abi.encode(0)});
     }
 }
