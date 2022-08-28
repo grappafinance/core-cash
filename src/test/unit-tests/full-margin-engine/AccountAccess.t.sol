4e0acb61e2e57cb2ca2d2bede6ba3e3135d8eea9
Anton Cheng <antonassocareer@gmail.com>
2022-08-28 14:57:39 +0800

    test: access control tests

---
+++ src/test/unit-tests/full-margin-engine/AccountAccess.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// import test base and helpers.
+import {FullMarginFixture} from "../../shared/FullMarginFixture.t.sol";
+
+import "../../../config/types.sol";
+import "../../../config/errors.sol";
+
+contract AdvancedMarginEngineAccessTest is FullMarginFixture {
+    uint256 private depositAmount = 100 * 1e6;
+
+    address private subAccountIdToModify;
+
+    function setUp() public {
+        usdc.mint(address(this), 1000_000 * 1e6);
+        usdc.approve(address(fmEngine), type(uint256).max);
+
+        subAccountIdToModify = address(uint160(alice) ^ uint160(1));
+    }
+
+    function testTransferAccount() public {
+        vm.startPrank(alice);
+        fmEngine.transferAccount(subAccountIdToModify, address(this));
+        vm.stopPrank();
+
+        // can access subaccount!
+        _assertCanAccessAccount(address(this), true);
+    }
+
+    function testCannotTransferToOverrideAnotherAccount() public {
+        // write something to account "address(this)"
+        _assertCanAccessAccount(address(this), true);
+
+        vm.startPrank(alice);
+        vm.expectRevert(AM_AccountIsNotEmpty.selector);
+        fmEngine.transferAccount(subAccountIdToModify, address(this));
+        vm.stopPrank();
+    }
+
+    function _assertCanAccessAccount(address subAccountId, bool _canAccess) internal {
+        // we can update the account now
+        ActionArgs[] memory actions = new ActionArgs[](1);
+        actions[0] = createAddCollateralAction(usdcId, address(this), depositAmount);
+
+        if (!_canAccess) vm.expectRevert(NoAccess.selector);
+
+        grappa.execute(fmEngineId, subAccountId, actions);
+    }
+}
