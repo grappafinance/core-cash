4366dc451ef9174701976ff116338ca22c0c2665
Anton Cheng <antonassocareer@gmail.com>
2022-09-01 10:56:06 +0800

    test: base engine flow tests

---
+++ src/test/engine-integrations/base-engine/BaseEngineSetup.t.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+import "../../mocks/MockERC20.sol";
+import "../../mocks/MockOracle.sol";
+import "../../mocks/MockChainlinkAggregator.sol";
+import "../../mocks/MockEngine.sol";
+
+// import "../../../core/engines/.sol";
+import "../../../core/engines/advanced-margin/VolOracle.sol";
+import "../../../core/Grappa.sol";
+import "../../../core/OptionToken.sol";
+
+import "../../../config/enums.sol";
+import "../../../config/types.sol";
+
+import "../../utils/Utilities.sol";
+
+import {ActionHelper} from "../../shared/ActionHelper.sol";
+
+// solhint-disable max-states-count
+
+abstract contract BaseEngineSetup is Test, ActionHelper, Utilities {
+    MockEngine internal engine;
+    Grappa internal grappa;
+    OptionToken internal option;
+
+    MockERC20 internal usdc;
+    MockERC20 internal weth;
+
+    MockOracle internal oracle;
+
+    // usdc collateralized call / put
+    uint32 internal productId;
+
+    // eth collateralized call / put
+    uint32 internal productIdEthCollat;
+
+    uint8 internal usdcId;
+    uint8 internal wethId;
+
+    uint8 internal engineId;
+
+    constructor() {
+        usdc = new MockERC20("USDC", "USDC", 6); // nonce: 1
+
+        weth = new MockERC20("WETH", "WETH", 18); // nonce: 2
+
+        oracle = new MockOracle(); // nonce: 3
+
+        // predit address of margin account and use it here
+        address grappaAddr = predictAddress(address(this), 5);
+
+        option = new OptionToken(grappaAddr); // nonce: 4
+
+        grappa = new Grappa(address(option), address(oracle)); // nonce: 5
+
+        
+        engine = new MockEngine(address(grappa), address(option)); // nonce 6
+
+        // register products
+        usdcId = grappa.registerAsset(address(usdc));
+        wethId = grappa.registerAsset(address(weth));
+
+        engineId = grappa.registerEngine(address(engine));
+
+        // engineId = grappa.registerEngine(address(engine));
+
+        productId = grappa.getProductId(engineId, address(weth), address(usdc), address(usdc));
+        productIdEthCollat = grappa.getProductId(engineId, address(weth), address(usdc), address(weth));
+    }
+
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+}
--- src/test/mocks/MockEngine.sol
+++ src/test/mocks/MockEngine.sol
@@ -4,94 +4,111 @@
 pragma solidity ^0.8.13;
 
 import {IMarginEngine} from "../../interfaces/IMarginEngine.sol";
+import {BaseEngine} from "../../core/engines/BaseEngine.sol";
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
 import "../../config/enums.sol";
 
-// contract MockEngine is IMarginEngine {
-//     bool public isSafe;
-//     uint256 public minCollateral;
-//     using SafeERC20 for IERC20;
+/**
+ * Mocked implementation to test base engine tx flow
+ */
+contract MockEngine is BaseEngine {
 
-//     function isAccountHealthy(
-//         address /*_subAccount*/
-//     ) external view returns (bool) {
-//         return isSafe;
-//     }
+    bool public isAboveWater;
 
-//     function setIsAccountSafe(bool _safe) external {
-//         isSafe = _safe;
-//     }
+    constructor(address _grappa, address _option) BaseEngine(_grappa, _option) {}
 
-//     function getMinCollateral(
-//         address /*_subAccount*/
-//     ) external view returns (uint256) {
-//         return minCollateral;
-//     }
+    function setIsAboveWater(bool _isAboveWater) external {
+      isAboveWater = _isAboveWater;
+    }
 
-//     function increaseCollateral(
-//         address, /*_subAccount*/
-//         address _from,
-//         address _collateral,
-//         uint8, /*_collateralId*/
-//         uint80 _amount
-//     ) external {
-//         IERC20(_collateral).safeTransferFrom(_from, address(this), _amount);
-//     }
+    function payCashValue(
+        address _asset,
+        address _recipient,
+        uint256 _amount
+    ) public override {
+        BaseEngine.payCashValue(_asset, _recipient, _amount);
+    }
 
-//     function decreaseCollateral(
-//         address, /*_subAccount*/
-//         address _to,
-//         address _collateral,
-//         uint8, /* _collateralId*/
-//         uint80 _amount
-//     ) external {
-//         IERC20(_collateral).safeTransfer(_to, _amount);
-//     }
+    /** ========================================================= **
+     *               Override Sate changing functions             *
+     ** ========================================================= **/
 
-//     function increaseDebt(
-//         address _subAccount,
-//         uint256 optionId,
-//         uint64 amount
-//     ) external {
-//         // do nothing
-//     }
+    function _addCollateralToAccount(
+        address _subAccount,
+        uint8 collateralId,
+        uint80 amount
+    ) internal override {}
 
-//     function decreaseDebt(
-//         address _subAccount,
-//         uint256 optionId,
-//         uint64 amount
-//     ) external {
-//         // do nothing
-//     }
+    function _removeCollateralFromAccount(
+        address _subAccount,
+        uint8 collateralId,
+        uint80 amount
+    ) internal override {}
 
-//     function merge(address _subAccount, uint256 _optionId) external returns (uint64 burnAmount) {
-//         // do nothing
-//     }
+    function _increaseShortInAccount(
+        address _subAccount,
+        uint256 tokenId,
+        uint64 amount
+    ) internal override {}
 
-//     function split(address _subAccount, uint256 _spreadId) external returns (uint256 optionId, uint64 mintAmount) {
-//         // do nothing
-//     }
+    function _decreaseShortInAccount(
+        address _subAccount,
+        uint256 tokenId,
+        uint64 amount
+    ) internal override {}
 
-//     function liquidate(
-//         address _subAccount,
-//         address _liquidator,
-//         uint256[] memory tokensToBurn,
-//         uint256[] memory amountsToBurn
-//     ) external returns (address collateral, uint80 amountToPay) {
-//         // do nothing
-//     }
+    function _mergeLongIntoSpread(
+        address _subAccount,
+        uint256 shortTokenId,
+        uint256 longTokenId,
+        uint64 amount
+    ) internal override {}
 
-//     function payCashValue(
-//         address _asset,
-//         address _recipient,
-//         uint256 _amount
-//     ) external {
-//         IERC20(_asset).safeTransfer(_recipient, _amount);
-//     }
+    function _splitSpreadInAccount(
+        address _subAccount,
+        uint256 spreadId,
+        uint64 amount
+    ) internal override {}
 
-//     function settleAtExpiry(address _subAccount) external {
-//         // do nothing
-//     }
-// }
+    function _addOptionToAccount(
+        address, /**_subAccount**/
+        uint256, /**tokenId**/
+        uint64 /**amount**/
+    ) internal pure override {}
+
+    function _removeOptionfromAccount(
+        address, /**_subAccount**/
+        uint256, /**tokenId**/
+        uint64 /**amount**/
+    ) internal pure override {}
+
+    function _settleAccount(address _subAccount, uint80 payout) internal override {}
+
+    /** ========================================================= **
+                    Override view functions for BaseEngine
+     ** ========================================================= **/
+
+    function _isAccountAboveWater(address /*_subAccount*/) internal view override returns (bool isHealthy) {
+        return isAboveWater;
+    }
+
+    function _getAccountPayout(address /*_subAccount*/) internal pure override returns (uint80) {
+        return 0;
+    }
+
+    function _verifyLongTokenIdToAdd(
+        uint256 /**_tokenId**/
+    ) internal pure override {}
+
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+}
