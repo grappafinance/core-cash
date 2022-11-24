78f2346e426e9d8271d85ca3b5af64fe305b0d8e
Michael Fletcher <36506122+Fletch153@users.noreply.github.com>
2022-11-24 01:34:56 +0000

    Added remove multiple collateral test (#161)
    * Added remove multiple collateral test
    
    * Extra test condition for RemoveCollateral test

--- src/test/engine-integrations/cross-margin/RemoveCollateral.t.sol
+++ src/test/engine-integrations/cross-margin/RemoveCollateral.t.sol
@@ -80,4 +80,95 @@ contract TestRemoveCollateral_CM is CrossMarginFixture {
         vm.expectRevert(stdError.arithmeticError);
         engine.execute(address(this), actions);
     }
+
+    function testMultipleCollateralsAddRemove() public {
+        uint256 depositAmountUSDC = 1 * 1e6;
+        uint256 depositAmountETH = 1 * 1e18;
+
+        // remove the initial deposit amount from setup
+        ActionArgs[] memory actions = new ActionArgs[](1);
+        actions[0] = createRemoveCollateralAction(depositAmount, usdcId, address(this));
+        engine.execute(address(this), actions);
+
+        (,, Balance[] memory collaterals) = engine.marginAccounts(address(this));
+
+        // no collaterals should remain from setup
+        assertEq(collaterals.length, 0);
+
+        //Add each collateral twice
+        actions = new ActionArgs[](4);
+        actions[0] = createAddCollateralAction(usdcId, address(this), depositAmountUSDC);
+        actions[1] = createAddCollateralAction(wethId, address(this), depositAmountETH);
+        actions[2] = createAddCollateralAction(usdcId, address(this), depositAmountUSDC);
+        actions[3] = createAddCollateralAction(wethId, address(this), depositAmountETH);
+
+        engine.execute(address(this), actions);
+
+        (,, collaterals) = engine.marginAccounts(address(this));
+
+        // check amounts
+        assertEq(collaterals[0].collateralId, usdcId);
+        assertEq(collaterals[0].amount, 2 * depositAmountUSDC);
+        assertEq(collaterals[1].collateralId, wethId);
+        assertEq(collaterals[1].amount, 2 * depositAmountETH);
+
+        // we should have 2 instead of 4 array slots
+        assertEq(collaterals.length, 2);
+
+        // remove half of each collateral
+        actions = new ActionArgs[](2);
+        actions[0] = createRemoveCollateralAction(depositAmountETH, wethId, address(this));
+        actions[1] = createRemoveCollateralAction(depositAmountUSDC, usdcId, address(this));
+        engine.execute(address(this), actions);
+
+        (,, collaterals) = engine.marginAccounts(address(this));
+
+        // check half has been removed
+        assertEq(collaterals[0].collateralId, usdcId);
+        assertEq(collaterals[0].amount, depositAmountUSDC);
+        assertEq(collaterals[1].collateralId, wethId);
+        assertEq(collaterals[1].amount, depositAmountETH);
+
+        // remove the remaining USDC
+        actions = new ActionArgs[](1);
+        actions[0] = createRemoveCollateralAction(depositAmountUSDC, usdcId, address(this));
+        engine.execute(address(this), actions);
+
+        (,, collaterals) = engine.marginAccounts(address(this));
+
+        // check remaining is now eth and amount matches
+        assertEq(collaterals[0].collateralId, wethId);
+        assertEq(collaterals[0].amount, depositAmountETH);
+
+        // USDC should have been removed from the array
+        assertEq(collaterals.length, 1);
+
+        // add both collaterals again
+        actions = new ActionArgs[](2);
+        actions[0] = createAddCollateralAction(usdcId, address(this), depositAmountUSDC);
+        actions[1] = createAddCollateralAction(wethId, address(this), depositAmountETH);
+        engine.execute(address(this), actions);
+
+        (,, collaterals) = engine.marginAccounts(address(this));
+
+        // collaterals should have reversed position since usdc was removed and re-added
+        assertEq(collaterals[0].collateralId, wethId);
+        assertEq(collaterals[0].amount, depositAmountETH * 2);
+        assertEq(collaterals[1].collateralId, usdcId);
+        assertEq(collaterals[1].amount, depositAmountUSDC);
+
+        // remove all collaterals
+        actions = new ActionArgs[](1);
+        actions[0] = createRemoveCollateralAction(depositAmountUSDC, usdcId, address(this));
+        engine.execute(address(this), actions);
+
+        (,, collaterals) = engine.marginAccounts(address(this));
+
+        // only eth left
+        assertEq(collaterals.length, 1);
+
+        // check half has been removed
+        assertEq(collaterals[0].collateralId, wethId);
+        assertEq(collaterals[0].amount, depositAmountETH * 2);
+    }
 }
