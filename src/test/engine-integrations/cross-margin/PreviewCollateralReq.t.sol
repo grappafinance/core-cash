248efaae65c2db9059ce8fe25871633fdd4e5de6
Fletch153 <michael.fletcher153@gmail.com>
2022-12-05 20:52:20 +0000

    Updated test as per PR

--- src/test/engine-integrations/cross-margin/PreviewCollateralReq.t.sol
+++ src/test/engine-integrations/cross-margin/PreviewCollateralReq.t.sol
@@ -447,15 +447,15 @@ contract PreviewCollateralReq_CMM is PreviewCollateralReqBase {
 
     function testPutGreaterThanCalls() public {
         OptionPosition[] memory positions = new OptionPosition[](4);
-        positions[0] = OptionPosition(TokenType.CALL, 300 * UNIT, 1 * sUNIT);
-        positions[1] = OptionPosition(TokenType.CALL, 200 * UNIT, -1 * sUNIT);
-        positions[2] = OptionPosition(TokenType.PUT, 2500 * UNIT, -1 * sUNIT);
-        positions[3] = OptionPosition(TokenType.PUT, 100 * UNIT, 1 * sUNIT);
+        positions[0] = OptionPosition(TokenType.CALL, 23000 * UNIT, 1 * sUNIT);
+        positions[1] = OptionPosition(TokenType.CALL, 22000 * UNIT, -1 * sUNIT);
+        positions[2] = OptionPosition(TokenType.PUT, 25000 * UNIT, -1 * sUNIT);
+        positions[3] = OptionPosition(TokenType.PUT, 10000 * UNIT, 1 * sUNIT);
 
         Balance[] memory balances = _previewMinCollateral(positions);
 
         assertEq(balances.length, 1);
         assertEq(balances[0].collateralId, usdcId);
-        assertEq(balances[0].amount, 2400 * UNIT);
+        assertEq(balances[0].amount, 15000 * UNIT);
     }
 }
