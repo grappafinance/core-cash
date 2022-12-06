63dd6e2c61e32fa6411348f4f4d156c8a84ad248
Anton Cheng <antonassocareer@gmail.com>
2022-12-06 10:29:18 +0800

    chore: update description

--- src/core/GrappaProxy.sol
+++ src/core/GrappaProxy.sol
@@ -5,7 +5,7 @@ import {ERC1967Proxy} from "openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
 
 /**
  * @notice  GrappaProxy is ERC1967Proxy
- * @dev     explicitly declare a file contract here to increase readability
+ * @dev     explicitly declare a contract here to increase readability
  */
 contract GrappaProxy is ERC1967Proxy {
     constructor(address _logic, bytes memory _data) payable ERC1967Proxy(_logic, _data) {}
--- src/core/engines/cross-margin/CrossMarginEngineProxy.sol
+++ src/core/engines/cross-margin/CrossMarginEngineProxy.sol
@@ -5,7 +5,7 @@ import {ERC1967Proxy} from "openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
 
 /**
  * @notice  CrossMarginEngineProxy is ERC1967Proxy
- * @dev     explicitly declare a file contract here to increase readability
+ * @dev     explicitly declare a contract here to increase readability
  */
 contract CrossMarginEngineProxy is ERC1967Proxy {
     constructor(address _logic, bytes memory _data) payable ERC1967Proxy(_logic, _data) {}
