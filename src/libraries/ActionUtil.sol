b71c0452594fd23bd08a6c374c9ed1f829320a02
Anton Cheng <antonassocareer@gmail.com>
2022-12-22 10:38:07 +0800

    fix: add back action util libs (#187)
    * chore: add back action utils
    
    * chore: lint
    
    * chore: snapshot

--- src/libraries/ActionUtil.sol
+++ src/libraries/ActionUtil.sol
@@ -169,6 +169,50 @@ library ActionUtil {
         action = ActionArgs({action: ActionType.SettleAccount, data: abi.encode(0)});
     }
 
+    function concat(ActionArgs[] memory x, ActionArgs[] memory v) internal pure returns (ActionArgs[] memory y) {
+        y = new ActionArgs[](x.length + v.length);
+        uint256 z;
+        uint256 i;
+        for (i; i < x.length;) {
+            y[z] = x[i];
+            unchecked {
+                ++z;
+                ++i;
+            }
+        }
+        for (i = 0; i < v.length;) {
+            y[z] = v[i];
+            unchecked {
+                ++z;
+                ++i;
+            }
+        }
+    }
+
+    function append(ActionArgs[] memory x, ActionArgs memory v) internal pure returns (ActionArgs[] memory y) {
+        y = new ActionArgs[](x.length + 1);
+        uint256 i;
+        for (i; i < x.length;) {
+            y[i] = x[i];
+            unchecked {
+                ++i;
+            }
+        }
+        y[i] = v;
+    }
+
+    function append(BatchExecute[] memory x, BatchExecute memory v) internal pure returns (BatchExecute[] memory y) {
+        y = new BatchExecute[](x.length + 1);
+        uint256 i;
+        for (i; i < x.length;) {
+            y[i] = x[i];
+            unchecked {
+                ++i;
+            }
+        }
+        y[i] = v;
+    }
+
     // add a function prefixed with test here so forge coverage will ignore this file
     function testChillOnHelper() public {}
 }
