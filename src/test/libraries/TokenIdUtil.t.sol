3bb1b9955a637cffcabee9db7bc0783e41c33af2
dsshap <dss.shapiro@gmail.com>
2023-01-27 14:45:37 -0600

    removing tokenId unused functions, snapshot

--- src/test/libraries/TokenIdUtil.t.sol
+++ src/test/libraries/TokenIdUtil.t.sol
@@ -21,23 +21,8 @@ contract TokenIdUtilTester {
         return result;
     }
 
-    function parseCompressedTokenId(uint192 tokenId) external pure returns (TokenType, uint40, uint64, uint64) {
-        (TokenType tokenType, uint40 productId, uint64 expiry, uint64 longStrike) = TokenIdUtil.parseCompressedTokenId(tokenId);
-        return (tokenType, productId, expiry, longStrike);
-    }
-
     function isExpired(uint256 tokenId) external view returns (bool expired) {
         bool result = TokenIdUtil.isExpired(tokenId);
-        return result;
-    }
-
-    function compress(uint256 _tokenId) external pure returns (uint192) {
-        uint192 result = TokenIdUtil.compress(_tokenId);
-        return result;
-    }
-
-    function expand(uint192 _tokenId) external pure returns (uint256 newId) {
-        uint256 result = TokenIdUtil.expand(_tokenId);
         return result;
     }
 }
@@ -52,28 +37,6 @@ contract TokenIdLibTest is Test {
 
     function setUp() public {
         tester = new TokenIdUtilTester();
-    }
-
-    function testCompressAndExpandAreMirrored(uint8 tokenType, uint40 productId, uint256 expiry, uint256 longStrike) public {
-        vm.assume(tokenType < 4);
-        vm.assume(productId > 0);
-        vm.assume(longStrike < type(uint64).max);
-        vm.assume(expiry < type(uint64).max);
-
-        // generate an Id without short strike
-        uint256 vanillaId = tester.getTokenId(TokenType(tokenType), productId, uint64(expiry), uint64(longStrike), 0);
-        uint192 compressedId = tester.compress(vanillaId);
-
-        (TokenType _tokenType, uint40 _productId, uint64 _expiry, uint64 _longStrike) =
-            tester.parseCompressedTokenId(compressedId);
-        assertEq(uint8(_tokenType), uint8(tokenType));
-        assertEq(_productId, productId);
-        assertEq(_expiry, expiry);
-        assertEq(_longStrike, longStrike);
-
-        uint256 expanded = tester.expand(compressedId);
-
-        assertEq(expanded, vanillaId);
     }
 
     function testIsExpired() public {
