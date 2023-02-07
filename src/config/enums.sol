10e2bf3a78b185f1ef6bee29a9bea5a46c83251c
Anton Cheng <antonassocareer@gmail.com>
2023-02-07 14:46:01 +0700

    chore: add comments on actions

--- src/config/enums.sol
+++ src/config/enums.sol
@@ -9,21 +9,24 @@ enum TokenType {
 }
 
 /**
- * @dev action types
+ * @dev common action types on margin engines
  */
 enum ActionType {
     AddCollateral,
     RemoveCollateral,
     MintShort,
     BurnShort,
-    MergeOptionToken,
-    SplitOptionToken,
     AddLong,
     RemoveLong,
     SettleAccount,
-    // actions that influece more than one subAccounts:
+    // actions related to converting between vanilla option and spread position
+    // These actions are defined in "DebitSpread"
+    MergeOptionToken,
+    SplitOptionToken,
+    // actions that influence more than one subAccounts:
+    // These actions are defined in "BaseEngineTransferable"
     MintShortIntoAccount, // increase short (debt) position in one subAccount, increase long token directly to another subAccount
-    TransferCollateral, // transfer collateral direclty to another subAccount
+    TransferCollateral, // transfer collateral directly to another subAccount
     TransferLong, // transfer long directly to another subAccount
     TransferShort // transfer short directly to another subAccount
 }
