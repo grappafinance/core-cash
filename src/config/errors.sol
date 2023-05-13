7e997bbec15db9dc42e2d2c4339b3341ea632999
Anton Cheng <antonassocareer@gmail.com>
2023-05-13 11:19:24 +0800

    chore: remove advanced-margin

--- src/config/errors.sol
+++ src/config/errors.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 // for easier import
 import "../core/oracles/errors.sol";
-import "../core/engines/advanced-margin/errors.sol";
+
 import "../core/engines/cross-margin/errors.sol";
 
 /* ------------------------ *
