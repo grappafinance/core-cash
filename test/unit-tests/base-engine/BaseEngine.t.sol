83f98ea146054d2fc6eda4dfcd87ee4916934116
Anton Cheng <antonassocareer@gmail.com>
2023-06-22 23:56:26 +0800

    test: full coverage on BaseEngine

--- test/unit-tests/base-engine/BaseEngine.t.sol
+++ test/unit-tests/base-engine/BaseEngine.t.sol
@@ -26,6 +26,22 @@ contract BaseEngineTest is MockedBaseEngineSetup {
         assertEq(engine.allowedExecutionLeft(mask, delegate), 9);
     }
 
+    function testSetAccessForSubAccounts() public {
+        engine.setAccountAccess(delegate, 10);
+
+        engine.setIsAboveWater(true);
+
+        vm.prank(delegate);
+        ActionArgs[] memory actions = new ActionArgs[](0);
+
+        // can operate on subaccount
+        address subAccount = address(uint160(address(this)) - 1);
+        engine.execute(subAccount, actions);
+
+        uint160 mask = uint160(address(this)) | 0xFF;
+        assertEq(engine.allowedExecutionLeft(mask, delegate), 9);
+    }
+
     function testRevokeAccess() public {
         engine.setAccountAccess(delegate, 10);
 
@@ -35,5 +51,25 @@ contract BaseEngineTest is MockedBaseEngineSetup {
         vm.expectRevert(NoAccess.selector);
         ActionArgs[] memory actions = new ActionArgs[](0);
         engine.execute(address(this), actions);
+    }
+
+    function testCanRequestPayoutFromGrappa() public {
+        usdc.mint(address(engine), 1000 * UNIT);
+
+        vm.prank(address(grappa));
+        engine.payCashValue(address(usdc), address(this), 1000 * UNIT);
+
+        assertEq(usdc.balanceOf(address(this)), 1000 * UNIT);
+    }
+
+    function testCannotRequestPayoutFromRandomAddress() public {
+        vm.expectRevert(NoAccess.selector);
+        engine.payCashValue(address(usdc), address(this), 1000 * UNIT);
+    }
+
+    // just for coverage
+    function testOnReceive() public {
+        uint256[] memory data = new uint256[](0);
+        assertEq(engine.onERC1155BatchReceived(address(0), address(0), data, data, ""), engine.onERC1155BatchReceived.selector);
     }
 }
